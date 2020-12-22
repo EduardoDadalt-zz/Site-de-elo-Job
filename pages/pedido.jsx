11a5d5d9d60002873c7fce7a3342e28e77ec4290
EduardoDadalt <54222234+EduardoDadalt@users.noreply.github.com>
2020-12-22 01:49:37 -0300

    Almost Ready

--- pages/pedido.jsx
+++ pages/pedido.jsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import useSWR from "swr";
 import fire, { database } from "../config/fire";
 import Image from "next/image";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { Auth } from "../context/auth";
+import SingIn from "../components/Login/SingIn";
 const getDataFromDatabase = async (url) => {
   return (await database.ref(url).once("value", (e) => e.val())).toJSON();
 };
 
 const Pedido = () => {
+  const {user} = useContext(Auth)
   const { data, error } = useSWR(
-    "/elojob" + fire.auth().currentUser.uid,
+    "/elojob" + (user&&user.uid),
     getDataFromDatabase
   );
+  if(!user){
+    return <Container>
+      <SingIn/>
+    </Container>
+  }
   if (error) {
     return <div>Deu Erro...</div>;
   }
   if (!data) {
-    return <div>Carregando...</div>;
+    return <Container>Carregando...</Container>;
   }
   return (
     <div>
